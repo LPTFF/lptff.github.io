f8d302bbd53637312faaecf4fb8d2514ee120625
LPTFF <lptffhh@gmail.com>
2023-04-23 22:44:47 +0800

    删除全局样式

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import './assets/main.css'
+// import './assets/main.css'
 import router from './router'
 
 createApp(App).use(router).mount('#app')
