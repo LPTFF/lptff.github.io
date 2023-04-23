ee38061b335c680e6ceb80809daee9014f3d7908
LPTFF <lptffhh@gmail.com>
2023-04-23 14:32:19 +0800

    设置主题页面

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ const NotFoundComponent = { template: '<p>Page not found</p>' }
 
 // 创建一个Vue 应用
 const routes = [
+    { path: '/:catchAll(.*)', name: 'Home', component: () => import('../views/home/index.vue') },
     { path: '/foo', component: NotFoundComponent },
     { path: '/Job', name: 'Job', component: () => import('../views/job/index.vue') },
     { path: '/Life', name: 'Life', component: () => import('../views/life/index.vue') }
---
+++ src/views/home/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="wrapper" @click="routePush('Life')">life</div>
+  <div class="wrapper" @click="routePush('Job')">job</div>
+</template>
+<script >
+import { useRouter } from "vue-router";
+export default {
+  data() {
+    return {};
+  },
+  setup() {
+    const route = useRouter();
+    console.log("route", route);
+    const goToOtherPage = (pageName, value) => {
+      console.log("233", pageName, value);
+      route.push({
+        name: pageName,
+        query: {
+          value: value,
+        },
+      });
+    };
+    function routePush(pageName) {
+      let value = "0";
+      this.goToOtherPage(pageName, value);
+    }
+    return { route, routePush, goToOtherPage };
+  },
+};
+</script>
+<style scoped>
+.wrapper {
+  font-size: 50px;
+}
+</style>
