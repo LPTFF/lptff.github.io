4a1b720891d1cdd8b6a05d8720907e2814d15c3a
LPTFF <lptffhh@gmail.com>
2023-04-23 15:29:58 +0800

    完善打包配置

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,36 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import vue from '@vitejs/plugin-vue';
+function pathResolve(dir) {
+  return resolve(__dirname, ".", dir);
+}
 
 export default defineConfig({
+  base: "",
   plugins: [vue()],
-  resolve: {},
-  port: 8080,
-})
+  resolve: {
+    alias: {
+      "/@": pathResolve("src"),
+    }
+  },
+  optimizeDeps: {
+    include: ['axios'],
+  },
+  build: {
+    target: 'modules',
+    outDir: 'dist',
+    assetsDir: 'assets',
+    minify: 'terser' // 混淆器
+  },
+  server: {
+    cors: true,
+    open: true,
+    port: 8080,
+    proxy: {
+      '/api': {
+        target: 'http://192.168.99.223:3000',   //代理接口
+        changeOrigin: true
+      }
+    }
+  }
+});
