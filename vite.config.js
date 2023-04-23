d30f8dfd8a1e5b2b1bea90880f929d515fe45f23
LPTFF <lptffhh@gmail.com>
2023-04-23 14:50:23 +0800

    解决白屏方案

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,6 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  alias: {
-    'vue': 'vue/dist/vue.esm-bundler.js' // 定义vue的别名，如果使用其他的插件，可能会用到别名
-  },
+  resolve: {},
   port: 8080,
 })
