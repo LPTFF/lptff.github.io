dec61a89f86b0f9f04624600cd6311ecd04c2a1f
LPTFF <lptffhh@gmail.com>
2023-04-23 22:35:12 +0800

    修复打包路径

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/github-pages/' : './',
+  base: './',
   plugins: [vue()],
   server: {
     cors: true,
