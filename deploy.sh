e2b937ccff7d862db4775a2e4c8860af73066ddc
LPTFF <lptffhh@gmail.com>
2023-05-16 13:29:26 +0800

    优化gitee平台发布配置

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,9 @@
 # 确保脚本抛出遇到的错误
 set -e
 
+# 环境集成
+npm i
+
 # 生成静态文件
 npm run build
 
@@ -17,4 +20,4 @@ git add -A
 git commit -m 'deploy'
 
 # 发布
-git push -f "https://gitee.com/love-tff/lptff.github.io.git" HEAD:gitee-pages
+git push -f "https://gitee.com/love-tff/love-tff.git" HEAD:gitee-pages
