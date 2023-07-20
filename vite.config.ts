c9ed2d1ea272004b0bfce5b563bac2fef627179f
lptff <lptffhh@gmail.com>
2023-07-20 21:10:34 +0800

    尝试预渲染

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import vitePrerender from 'vite-plugin-prerender'
+import path from 'path'
 // import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
   // plugins: [vue(), visualizer()],
-  plugins: [vue()],
+  plugins: [vue(), vitePrerender({
+    staticDir: path.join(__dirname, 'dist'),
+    routes: ['/', '/home'],
+  }),],
   server: {
     cors: true,
     open: false,
