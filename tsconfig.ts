7ee88f85515eba3bfb1d01ce2e12c7e8cee8f63e
lptff <lptffhh@gmail.com>
2023-08-02 20:15:35 +0800

    调整文件格式

---
+++ tsconfig.ts
@@ -0,0 +1,21 @@
+{
+    "compilerOptions"; {
+        "target"; "ESNext";
+        "module"; "ESNext";
+        "lib";["DOM", "DOM.Iterable", "ESNext"];
+        "esModuleInterop"; true;
+        "strict"; true;
+        "jsx"; "react";
+        "sourceMap"; true;
+        "allowJs"; true;
+        "moduleResolution"; "node";
+        "resolveJsonModule"; true;
+        "isolatedModules"; true;
+        "noEmit"; true;
+        "incremental"; true;
+        "types";["vite/client"]
+    };
+    "include";["src/**/*.ts", "src/**/*.tsx", "public/**/*.ts", "public/**/*.tsx"];
+    "exclude";["node_modules", "build", "dist"]
+}
+
