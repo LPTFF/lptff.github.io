9a58b785923f7699569b290384a8a72a6f3d4cf3
lptff <lptffhh@gmail.com>
2023-12-27 19:22:44 +0800

    代码优化

--- src/views/home/advancedSearch/index.vue
+++ src/views/home/advancedSearch/index.vue
@@ -1,71 +1,11 @@
 <template>
   <div>
-    <div class="search-common">
-      <span class="search-title">限定关键词</span>
+    <div class="search-common" v-for="search in searches" :key="search.key">
+      <span class="search-title">{{ search.title }}</span>
       <el-input
-        v-model="findKeyWords"
-        placeholder="你还什么都没输入哦"
-        @keyup.enter="submitKey"
-      >
-        <template #prefix>
-          <el-icon><search /></el-icon>
-        </template>
-      </el-input>
-    </div>
-    <div class="search-common">
-      <span class="search-title">限定标题</span>
-      <el-input
-        v-model="findKeyWords"
-        placeholder="你还什么都没输入哦"
-        @keyup.enter="submitTitle"
-      >
-        <template #prefix>
-          <el-icon><search /></el-icon>
-        </template>
-      </el-input>
-    </div>
-    <div class="search-common">
-      <span class="search-title">限定内容关键字</span>
-      <el-input
-        v-model="findKeyWords"
-        placeholder="你还什么都没输入哦"
-        @keyup.enter="submitContent"
-      >
-        <template #prefix>
-          <el-icon><search /></el-icon>
-        </template>
-      </el-input>
-    </div>
-    <div class="search-common">
-      <span class="search-title">限定telegram关键词</span>
-      <el-input
-        v-model="findKeyWords"
-        placeholder="你还什么都没输入哦"
-        @keyup.enter="submitTelegramKeyWordsWebsite"
-      >
-        <template #prefix>
-          <el-icon><search /></el-icon>
-        </template>
-      </el-input>
-    </div>
-    <div class="search-common">
-      <span class="search-title">限定v2ex来源</span>
-      <el-input
-        v-model="findKeyWords"
-        placeholder="你还什么都没输入哦"
-        @keyup.enter="submitV2exSourceWebsite"
-      >
-        <template #prefix>
-          <el-icon><search /></el-icon>
-        </template>
-      </el-input>
-    </div>
-    <div class="search-common">
-      <span class="search-title">限定GitHub来源</span>
-      <el-input
-        v-model="findKeyWords"
-        placeholder="你还什么都没输入哦"
-        @keyup.enter="submitGitHubSourceWebsite"
+        v-model="search.model"
+        :placeholder="search.placeholder"
+        @keyup.enter="search.action"
       >
         <template #prefix>
           <el-icon><search /></el-icon>
@@ -99,47 +39,6 @@ enum WebsiteType {
 export default defineComponent({
   name: "App",
   setup() {
-    const findKeyWords = ref("");
-    const submitKey = () => {
-      const trimmedInput = findKeyWords.value.trim();
-      if (trimmedInput.length > 0) {
-        console.log("submitKey", trimmedInput);
-        let url = `https://www.google.com/search?q=%22${encodeURIComponent(
-          trimmedInput
-        )}%22&newwindow=1&sca_esv=583929409&sxsrf=AM9HkKlIEElwoDCc5zwouV7Ze8BsbcrNpQ:1700474180556&source=lnt&tbs=qdr:m&sa=X&ved=2ahUKEwjK9-z7p9KCAxVfke4BHRhlAAMQpwV6BAgBEBA&biw=1920&bih=931&dpr=1`;
-        gotoOutPage(url);
-      } else {
-        console.log("Input is empty");
-        findKeyWords.value = "";
-      }
-    };
-    const submitTitle = () => {
-      const trimmedInput = findKeyWords.value.trim();
-      if (trimmedInput.length > 0) {
-        console.log("submitTitle", trimmedInput);
-        let url = `https://www.google.com/search?q=intitle%3A${encodeURIComponent(
-          trimmedInput
-        )}&newwindow=1&sca_esv=583929409&biw=1920&bih=931&tbs=qdr%3Am&sxsrf=AM9HkKm4zY2SZ2kSHrbu1kXJrWAYkJObTw%3A1700474237633&ei=fS1bZe-mJtPnkPIP_p61iAE&ved=0ahUKEwiv2YiXqNKCAxXTM0QIHX5PDREQ4dUDCBA&uact=5&oq=intitle%3A%E9%9D%92%E9%BE%99%E8%84%9A%E6%9C%AC&gs_lp=Egxnd3Mtd2l6LXNlcnAiFGludGl0bGU66Z2S6b6Z6ISa5pysSPteUMoHWNtYcAF4ApABAJgB1wKgAcQVqgEFMi05LjG4AQPIAQD4AQHCAgQQABhHwgIKECMYgAQYigUYJ8ICBBAjGCfCAgoQABiABBiKBRhDwgIFEAAYgATCAggQABiABBiiBOIDBBgAIEGIBgGQBgo&sclient=gws-wiz-serp`;
-        gotoOutPage(url);
-      } else {
-        console.log("Input is empty");
-        findKeyWords.value = "";
-      }
-    };
-
-    const submitContent = () => {
-      const trimmedInput = findKeyWords.value.trim();
-      if (trimmedInput.length > 0) {
-        console.log("2333", trimmedInput);
-        let url = `https://www.google.com/search?q=intext%3A${encodeURIComponent(
-          trimmedInput
-        )}&newwindow=1&sca_esv=583929409&biw=1920&bih=931&tbs=qdr%3Am&sxsrf=AM9HkKm4zY2SZ2kSHrbu1kXJrWAYkJObTw%3A1700474237633&ei=fS1bZe-mJtPnkPIP_p61iAE&ved=0ahUKEwiv2YiXqNKCAxXTM0QIHX5PDREQ4dUDCBA&uact=5&oq=intitle%3A%E9%9D%92%E9%BE%99%E8%84%9A%E6%9C%AC&gs_lp=Egxnd3Mtd2l6LXNlcnAiFGludGl0bGU66Z2S6b6Z6ISa5pysSPteUMoHWNtYcAF4ApABAJgB1wKgAcQVqgEFMi05LjG4AQPIAQD4AQHCAgQQABhHwgIKECMYgAQYigUYJ8ICBBAjGCfCAgoQABiABBiKBRhDwgIFEAAYgATCAggQABiABBiiBOIDBBgAIEGIBgGQBgo&sclient=gws-wiz-serp`;
-        gotoOutPage(url);
-      } else {
-        console.log("Input is empty");
-        findKeyWords.value = "";
-      }
-    };
     const websiteTransformType = (type: any) => {
       switch (type) {
         case "success":
@@ -154,46 +53,137 @@ export default defineComponent({
           return WebsiteType.Default; // 默认类型
       }
     };
-    const submitTelegramKeyWordsWebsite = () => {
+    const findKeyWords = ref("");
+    const submitSearch = (baseUrl: any, siteQuery: any) => {
       const trimmedInput = findKeyWords.value.trim();
       if (trimmedInput.length > 0) {
-        console.log("2333", trimmedInput);
-
-        let url = `https://www.google.com/search?q=${encodeURIComponent(
-          trimmedInput
-        )}++inurl%3At.me&newwindow=1&sca_esv=583929409&biw=1920&bih=931&tbs=qdr%3Am&sxsrf=AM9HkKlAXizX6rJ8BLAR3lQm4I4aOZbGnQ%3A1700740893045&ei=HT9fZe6tApuN4-EP8IGb8Ac&ved=0ahUKEwiusKPGidqCAxWbxjgGHfDABn4Q4dUDCBA&uact=5&oq=%E8%B4%A6%E5%8F%B7%E6%89%B9%E5%8F%91++inurl%3At.me&gs_lp=Egxnd3Mtd2l6LXNlcnAiGOi0puWPt-aJueWPkSAgaW51cmw6dC5tZUiSI1D7Dlj8GXABeAGQAQCYAZoBoAGZCqoBAzAuObgBA8gBAPgBAcICChAAGEcY1gQYsAPiAwQYACBBiAYBkAYK&sclient=gws-wiz-serp`;
+        console.log("Search input:", trimmedInput);
+        let url = `${baseUrl}${encodeURIComponent(trimmedInput)}${siteQuery}`;
         gotoOutPage(url);
       } else {
         console.log("Input is empty");
         findKeyWords.value = "";
       }
+    };
+    const submitKey = () => {
+      const baseUrl = "https://www.google.com/search?q=%22";
+      const siteQuery =
+        "%22&newwindow=1&sca_esv=583929409&sxsrf=AM9HkKlIEElwoDCc5zwouV7Ze8BsbcrNpQ:1700474180556&source=lnt&tbs=qdr:m&sa=X&ved=2ahUKEwjK9-z7p9KCAxVfke4BHRhlAAMQpwV6BAgBEBA&biw=1920&bih=931&dpr=1";
+      submitSearch(baseUrl, siteQuery);
+    };
+    const submitTitle = () => {
+      const baseUrl = "https://www.google.com/search?q=intitle%3A";
+      const siteQuery =
+        "&newwindow=1&sca_esv=583929409&biw=1920&bih=931&tbs=qdr%3Am&sxsrf=AM9HkKm4zY2SZ2kSHrbu1kXJrWAYkJObTw%3A1700474237633&ei=fS1bZe-mJtPnkPIP_p61iAE&ved=0ahUKEwiv2YiXqNKCAxXTM0QIHX5PDREQ4dUDCBA&uact=5&oq=intitle%3A%E9%9D%92%E9%BE%99%E8%84%9A%E6%9C%AC&gs_lp=Egxnd3Mtd2l6LXNlcnAiFGludGl0bGU66Z2S6b6Z6ISa5pysSPteUMoHWNtYcAF4ApABAJgB1wKgAcQVqgEFMi05LjG4AQPIAQD4AQHCAgQQABhHwgIKECMYgAQYigUYJ8ICBBAjGCfCAgoQABiABBiKBRhDwgIFEAAYgATCAggQABiABBiiBOIDBBgAIEGIBgGQBgo&sclient=gws-wiz-serp";
+      submitSearch(baseUrl, siteQuery);
+    };
+    const submitContent = () => {
+      const baseUrl = "https://www.google.com/search?q=intext%3A";
+      const siteQuery =
+        "&newwindow=1&sca_esv=583929409&biw=1920&bih=931&tbs=qdr%3Am&sxsrf=AM9HkKm4zY2SZ2kSHrbu1kXJrWAYkJObTw%3A1700474237633&ei=fS1bZe-mJtPnkPIP_p61iAE&ved=0ahUKEwiv2YiXqNKCAxXTM0QIHX5PDREQ4dUDCBA&uact=5&oq=intitle%3A%E9%9D%92%E9%BE%99%E8%84%9A%E6%9C%AC&gs_lp=Egxnd3Mtd2l6LXNlcnAiFGludGl0bGU66Z2S6b6Z6ISa5pysSPteUMoHWNtYcAF4ApABAJgB1wKgAcQVqgEFMi05LjG4AQPIAQD4AQHCAgQQABhHwgIKECMYgAQYigUYJ8ICBBAjGCfCAgoQABiABBiKBRhDwgIFEAAYgATCAggQABiABBiiBOIDBBgAIEGIBgGQBgo&sclient=gws-wiz-serp";
+      submitSearch(baseUrl, siteQuery);
+    };
+    const submitTelegramKeyWordsWebsite = () => {
+      const baseUrl = "https://www.google.com/search?q=";
+      const siteQuery =
+        "+inurl%3At.me&newwindow=1&sca_esv=583929409&biw=1920&bih=931&tbs=qdr%3Am&sxsrf=AM9HkKlAXizX6rJ8BLAR3lQm4I4aOZbGnQ%3A1700740893045&ei=HT9fZe6tApuN4-EP8IGb8Ac&ved=0ahUKEwiusKPGidqCAxWbxjgGHfDABn4Q4dUDCBA&uact=5&oq=%E8%B4%A6%E5%8F%B7%E6%89%B9%E5%8F%91++inurl%3At.me&gs_lp=Egxnd3Mtd2l6LXNlcnAiGOi0puWPt-aJueWPkSAgaW51cmw6dC5tZUiSI1D7Dlj8GXABeAGQAQCYAZoBoAGZCqoBAzAuObgBA8gBAPgBAcICChAAGEcY1gQYsAPiAwQYACBBiAYBkAYK&sclient=gws-wiz-serp";
+      submitSearch(baseUrl, siteQuery);
+    };
+    const submitTelegramSourcesWebsite = () => {
+      const baseUrl = "https://www.google.com/search?q=";
+      const siteQuery =
+        "+site%3At.me&newwindow=1&sca_esv=583929409&biw=1920&bih=931&tbs=qdr%3Am&sxsrf=AM9HkKlAXizX6rJ8BLAR3lQm4I4aOZbGnQ%3A1700740893045&ei=HT9fZe6tApuN4-EP8IGb8Ac&ved=0ahUKEwiusKPGidqCAxWbxjgGHfDABn4Q4dUDCBA&uact=5&oq=%E8%B4%A6%E5%8F%B7%E6%89%B9%E5%8F%91++inurl%3At.me&gs_lp=Egxnd3Mtd2l6LXNlcnAiGOi0puWPt-aJueWPkSAgaW51cmw6dC5tZUiSI1D7Dlj8GXABeAGQAQCYAZoBoAGZCqoBAzAuObgBA8gBAPgBAcICChAAGEcY1gQYsAPiAwQYACBBiAYBkAYK&sclient=gws-wiz-serp";
+      submitSearch(baseUrl, siteQuery);
+    };
+    const submitV2exKeyWordsWebsite = () => {
+      const baseUrl = "https://www.google.com/search?q=";
+      const siteQuery =
+        "+inurl%3Av2ex.com&newwindow=1&sca_esv=581914905&sxsrf=AM9HkKmHk76oMnT8-gIP_y88N0m2pplZPQ:1699882201083&source=lnt&tbs=qdr:m&sa=X&ved=2ahUKEwiwrYXWisGCAxVbLTQIHdktAEsQpwV6BAgBEBA&biw=1920&bih=931&dpr=1";
+      submitSearch(baseUrl, siteQuery);
     };
     const submitV2exSourceWebsite = () => {
-      const trimmedInput = findKeyWords.value.trim();
-      if (trimmedInput.length > 0) {
-        console.log("2333", trimmedInput);
-        let url = `https://www.google.com/search?q=site:v2ex.com/t+${encodeURIComponent(
-          trimmedInput
-        )}&newwindow=1&sca_esv=581914905&sxsrf=AM9HkKmHk76oMnT8-gIP_y88N0m2pplZPQ:1699882201083&source=lnt&tbs=qdr:m&sa=X&ved=2ahUKEwiwrYXWisGCAxVbLTQIHdktAEsQpwV6BAgBEBA&biw=1920&bih=931&dpr=1`;
-        gotoOutPage(url);
-      } else {
-        console.log("Input is empty");
-        findKeyWords.value = "";
-      }
+      const baseUrl = "https://www.google.com/search?q=";
+      const siteQuery =
+        "+site%3Av2ex.com&newwindow=1&sca_esv=581914905&sxsrf=AM9HkKmHk76oMnT8-gIP_y88N0m2pplZPQ:1699882201083&source=lnt&tbs=qdr:m&sa=X&ved=2ahUKEwiwrYXWisGCAxVbLTQIHdktAEsQpwV6BAgBEBA&biw=1920&bih=931&dpr=1";
+      submitSearch(baseUrl, siteQuery);
+    };
+    const submitGitHubKeyWordsWebsite = () => {
+      const baseUrl = "https://www.google.com/search?q=";
+      const siteQuery =
+        "+inurl%3Agithub.com&newwindow=1&sca_esv=583929409&biw=1920&bih=931&tbs=qdr%3Am&sxsrf=AM9HkKkO7V2IJ5Ozp4j5EDuyuzBwn-ZMUA%3A1700478902629&ei=tj9bZaiBJrihkPIP9KiF4As&ved=0ahUKEwiotMHHudKCAxW4EEQIHXRUAbwQ4dUDCBA&uact=5&oq=%E9%9D%92%E9%BE%99%E8%84%9A%E6%9C%AC+site%3Agithub.com&gs_lp=Egxnd3Mtd2l6LXNlcnAiHOmdkum-meiEmuacrCBzaXRlOmdpdGh1Yi5jb21IxlxQpxFY6VZwAngBkAEAmAHAAqAB_RSqAQUyLTQuNbgBA8gBAPgBAcICChAAGEcY1gQYsAPiAwQYACBBiAYBkAYK&sclient=gws-wiz-serp";
+      submitSearch(baseUrl, siteQuery);
     };
     const submitGitHubSourceWebsite = () => {
-      const trimmedInput = findKeyWords.value.trim();
-      if (trimmedInput.length > 0) {
-        console.log("submitGitHubWebsite", trimmedInput);
-        let url = `https://www.google.com/search?q=${encodeURIComponent(
-          trimmedInput
-        )}+site%3Agithub.com&newwindow=1&sca_esv=583929409&biw=1920&bih=931&tbs=qdr%3Am&sxsrf=AM9HkKkO7V2IJ5Ozp4j5EDuyuzBwn-ZMUA%3A1700478902629&ei=tj9bZaiBJrihkPIP9KiF4As&ved=0ahUKEwiotMHHudKCAxW4EEQIHXRUAbwQ4dUDCBA&uact=5&oq=%E9%9D%92%E9%BE%99%E8%84%9A%E6%9C%AC+site%3Agithub.com&gs_lp=Egxnd3Mtd2l6LXNlcnAiHOmdkum-meiEmuacrCBzaXRlOmdpdGh1Yi5jb21IxlxQpxFY6VZwAngBkAEAmAHAAqAB_RSqAQUyLTQuNbgBA8gBAPgBAcICChAAGEcY1gQYsAPiAwQYACBBiAYBkAYK&sclient=gws-wiz-serp`;
-        gotoOutPage(url);
-      } else {
-        console.log("Input is empty");
-        findKeyWords.value = "";
-      }
+      const baseUrl = "https://www.google.com/search?q=";
+      const siteQuery =
+        "+site%3Agithub.com&newwindow=1&sca_esv=583929409&biw=1920&bih=931&tbs=qdr%3Am&sxsrf=AM9HkKkO7V2IJ5Ozp4j5EDuyuzBwn-ZMUA%3A1700478902629&ei=tj9bZaiBJrihkPIP9KiF4As&ved=0ahUKEwiotMHHudKCAxW4EEQIHXRUAbwQ4dUDCBA&uact=5&oq=%E9%9D%92%E9%BE%99%E8%84%9A%E6%9C%AC+site%3Agithub.com&gs_lp=Egxnd3Mtd2l6LXNlcnAiHOmdkum-meiEmuacrCBzaXRlOmdpdGh1Yi5jb21IxlxQpxFY6VZwAngBkAEAmAHAAqAB_RSqAQUyLTQuNbgBA8gBAPgBAcICChAAGEcY1gQYsAPiAwQYACBBiAYBkAYK&sclient=gws-wiz-serp";
+      submitSearch(baseUrl, siteQuery);
     };
+    const searches = ref([
+      {
+        key: "keyword",
+        title: "限定关键词",
+        model: findKeyWords,
+        placeholder: "你还什么都没输入哦",
+        action: submitKey,
+      },
+      {
+        key: "title",
+        title: "限定标题",
+        model: findKeyWords,
+        placeholder: "你还什么都没输入哦",
+        action: submitTitle,
+      },
+      {
+        key: "content",
+        title: "限定内容关键字",
+        model: findKeyWords,
+        placeholder: "你还什么都没输入哦",
+        action: submitContent,
+      },
+      {
+        key: "telegram",
+        title: "限定telegram关键词",
+        model: findKeyWords,
+        placeholder: "你还什么都没输入哦",
+        action: submitTelegramKeyWordsWebsite,
+      },
+      {
+        key: "telegram",
+        title: "限定telegram来源",
+        model: findKeyWords,
+        placeholder: "你还什么都没输入哦",
+        action: submitTelegramSourcesWebsite,
+      },
+      {
+        key: "v2ex",
+        title: "限定v2ex关键词",
+        model: findKeyWords,
+        placeholder: "你还什么都没输入哦",
+        action: submitV2exKeyWordsWebsite,
+      },
+      {
+        key: "v2ex",
+        title: "限定v2ex来源",
+        model: findKeyWords,
+        placeholder: "你还什么都没输入哦",
+        action: submitV2exSourceWebsite,
+      },
+      {
+        key: "GitHub",
+        title: "限定GitHub关键词",
+        model: findKeyWords,
+        placeholder: "你还什么都没输入哦",
+        action: submitGitHubKeyWordsWebsite,
+      },
+      {
+        key: "GitHub",
+        title: "限定GitHub来源",
+        model: findKeyWords,
+        placeholder: "你还什么都没输入哦",
+        action: submitGitHubSourceWebsite,
+      },
+    ]);
     return {
       findKeyWords,
       submitKey,
@@ -213,6 +203,7 @@ export default defineComponent({
         { text: "代理人抓包", type: "danger" },
       ],
       websiteTransformType,
+      searches,
     };
   },
   components: {
