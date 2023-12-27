b8b25b00ea89a49f3fb712addeafd22a5d727ecd
lptff <lptffhh@gmail.com>
2023-12-27 19:45:18 +0800

    新增搜索源

--- src/views/home/advancedSearch/index.vue
+++ src/views/home/advancedSearch/index.vue
@@ -17,7 +17,7 @@
       <el-tag
         v-for="(tag, index) in tags"
         :key="index"
-        class="ml-2"
+        class="ml-2 tag-common"
         :type="websiteTransformType(tag.type)"
         >{{ tag.text }}</el-tag
       >
@@ -65,58 +65,63 @@ export default defineComponent({
         findKeyWords.value = "";
       }
     };
+    const baseUrl = "https://www.google.com/search?q=";
     const submitKey = () => {
-      const baseUrl = "https://www.google.com/search?q=%22";
+      const submitKeyUrl = baseUrl + "%22";
       const siteQuery =
         "%22&newwindow=1&sca_esv=583929409&sxsrf=AM9HkKlIEElwoDCc5zwouV7Ze8BsbcrNpQ:1700474180556&source=lnt&tbs=qdr:m&sa=X&ved=2ahUKEwjK9-z7p9KCAxVfke4BHRhlAAMQpwV6BAgBEBA&biw=1920&bih=931&dpr=1";
-      submitSearch(baseUrl, siteQuery);
+      submitSearch(submitKeyUrl, siteQuery);
     };
     const submitTitle = () => {
-      const baseUrl = "https://www.google.com/search?q=intitle%3A";
+      const submitTitleUrl = baseUrl + "intitle%3A";
       const siteQuery =
         "&newwindow=1&sca_esv=583929409&biw=1920&bih=931&tbs=qdr%3Am&sxsrf=AM9HkKm4zY2SZ2kSHrbu1kXJrWAYkJObTw%3A1700474237633&ei=fS1bZe-mJtPnkPIP_p61iAE&ved=0ahUKEwiv2YiXqNKCAxXTM0QIHX5PDREQ4dUDCBA&uact=5&oq=intitle%3A%E9%9D%92%E9%BE%99%E8%84%9A%E6%9C%AC&gs_lp=Egxnd3Mtd2l6LXNlcnAiFGludGl0bGU66Z2S6b6Z6ISa5pysSPteUMoHWNtYcAF4ApABAJgB1wKgAcQVqgEFMi05LjG4AQPIAQD4AQHCAgQQABhHwgIKECMYgAQYigUYJ8ICBBAjGCfCAgoQABiABBiKBRhDwgIFEAAYgATCAggQABiABBiiBOIDBBgAIEGIBgGQBgo&sclient=gws-wiz-serp";
-      submitSearch(baseUrl, siteQuery);
+      submitSearch(submitTitleUrl, siteQuery);
     };
     const submitContent = () => {
-      const baseUrl = "https://www.google.com/search?q=intext%3A";
+      const submitContentUrl = baseUrl + "intext%3A";
       const siteQuery =
         "&newwindow=1&sca_esv=583929409&biw=1920&bih=931&tbs=qdr%3Am&sxsrf=AM9HkKm4zY2SZ2kSHrbu1kXJrWAYkJObTw%3A1700474237633&ei=fS1bZe-mJtPnkPIP_p61iAE&ved=0ahUKEwiv2YiXqNKCAxXTM0QIHX5PDREQ4dUDCBA&uact=5&oq=intitle%3A%E9%9D%92%E9%BE%99%E8%84%9A%E6%9C%AC&gs_lp=Egxnd3Mtd2l6LXNlcnAiFGludGl0bGU66Z2S6b6Z6ISa5pysSPteUMoHWNtYcAF4ApABAJgB1wKgAcQVqgEFMi05LjG4AQPIAQD4AQHCAgQQABhHwgIKECMYgAQYigUYJ8ICBBAjGCfCAgoQABiABBiKBRhDwgIFEAAYgATCAggQABiABBiiBOIDBBgAIEGIBgGQBgo&sclient=gws-wiz-serp";
-      submitSearch(baseUrl, siteQuery);
+      submitSearch(submitContentUrl, siteQuery);
     };
     const submitTelegramKeyWordsWebsite = () => {
-      const baseUrl = "https://www.google.com/search?q=";
       const siteQuery =
         "+inurl%3At.me&newwindow=1&sca_esv=583929409&biw=1920&bih=931&tbs=qdr%3Am&sxsrf=AM9HkKlAXizX6rJ8BLAR3lQm4I4aOZbGnQ%3A1700740893045&ei=HT9fZe6tApuN4-EP8IGb8Ac&ved=0ahUKEwiusKPGidqCAxWbxjgGHfDABn4Q4dUDCBA&uact=5&oq=%E8%B4%A6%E5%8F%B7%E6%89%B9%E5%8F%91++inurl%3At.me&gs_lp=Egxnd3Mtd2l6LXNlcnAiGOi0puWPt-aJueWPkSAgaW51cmw6dC5tZUiSI1D7Dlj8GXABeAGQAQCYAZoBoAGZCqoBAzAuObgBA8gBAPgBAcICChAAGEcY1gQYsAPiAwQYACBBiAYBkAYK&sclient=gws-wiz-serp";
       submitSearch(baseUrl, siteQuery);
     };
     const submitTelegramSourcesWebsite = () => {
-      const baseUrl = "https://www.google.com/search?q=";
       const siteQuery =
         "+site%3At.me&newwindow=1&sca_esv=583929409&biw=1920&bih=931&tbs=qdr%3Am&sxsrf=AM9HkKlAXizX6rJ8BLAR3lQm4I4aOZbGnQ%3A1700740893045&ei=HT9fZe6tApuN4-EP8IGb8Ac&ved=0ahUKEwiusKPGidqCAxWbxjgGHfDABn4Q4dUDCBA&uact=5&oq=%E8%B4%A6%E5%8F%B7%E6%89%B9%E5%8F%91++inurl%3At.me&gs_lp=Egxnd3Mtd2l6LXNlcnAiGOi0puWPt-aJueWPkSAgaW51cmw6dC5tZUiSI1D7Dlj8GXABeAGQAQCYAZoBoAGZCqoBAzAuObgBA8gBAPgBAcICChAAGEcY1gQYsAPiAwQYACBBiAYBkAYK&sclient=gws-wiz-serp";
       submitSearch(baseUrl, siteQuery);
     };
     const submitV2exKeyWordsWebsite = () => {
-      const baseUrl = "https://www.google.com/search?q=";
       const siteQuery =
         "+inurl%3Av2ex.com&newwindow=1&sca_esv=581914905&sxsrf=AM9HkKmHk76oMnT8-gIP_y88N0m2pplZPQ:1699882201083&source=lnt&tbs=qdr:m&sa=X&ved=2ahUKEwiwrYXWisGCAxVbLTQIHdktAEsQpwV6BAgBEBA&biw=1920&bih=931&dpr=1";
       submitSearch(baseUrl, siteQuery);
     };
     const submitV2exSourceWebsite = () => {
-      const baseUrl = "https://www.google.com/search?q=";
       const siteQuery =
         "+site%3Av2ex.com&newwindow=1&sca_esv=581914905&sxsrf=AM9HkKmHk76oMnT8-gIP_y88N0m2pplZPQ:1699882201083&source=lnt&tbs=qdr:m&sa=X&ved=2ahUKEwiwrYXWisGCAxVbLTQIHdktAEsQpwV6BAgBEBA&biw=1920&bih=931&dpr=1";
       submitSearch(baseUrl, siteQuery);
     };
     const submitGitHubKeyWordsWebsite = () => {
-      const baseUrl = "https://www.google.com/search?q=";
       const siteQuery =
         "+inurl%3Agithub.com&newwindow=1&sca_esv=583929409&biw=1920&bih=931&tbs=qdr%3Am&sxsrf=AM9HkKkO7V2IJ5Ozp4j5EDuyuzBwn-ZMUA%3A1700478902629&ei=tj9bZaiBJrihkPIP9KiF4As&ved=0ahUKEwiotMHHudKCAxW4EEQIHXRUAbwQ4dUDCBA&uact=5&oq=%E9%9D%92%E9%BE%99%E8%84%9A%E6%9C%AC+site%3Agithub.com&gs_lp=Egxnd3Mtd2l6LXNlcnAiHOmdkum-meiEmuacrCBzaXRlOmdpdGh1Yi5jb21IxlxQpxFY6VZwAngBkAEAmAHAAqAB_RSqAQUyLTQuNbgBA8gBAPgBAcICChAAGEcY1gQYsAPiAwQYACBBiAYBkAYK&sclient=gws-wiz-serp";
       submitSearch(baseUrl, siteQuery);
     };
     const submitGitHubSourceWebsite = () => {
-      const baseUrl = "https://www.google.com/search?q=";
       const siteQuery =
         "+site%3Agithub.com&newwindow=1&sca_esv=583929409&biw=1920&bih=931&tbs=qdr%3Am&sxsrf=AM9HkKkO7V2IJ5Ozp4j5EDuyuzBwn-ZMUA%3A1700478902629&ei=tj9bZaiBJrihkPIP9KiF4As&ved=0ahUKEwiotMHHudKCAxW4EEQIHXRUAbwQ4dUDCBA&uact=5&oq=%E9%9D%92%E9%BE%99%E8%84%9A%E6%9C%AC+site%3Agithub.com&gs_lp=Egxnd3Mtd2l6LXNlcnAiHOmdkum-meiEmuacrCBzaXRlOmdpdGh1Yi5jb21IxlxQpxFY6VZwAngBkAEAmAHAAqAB_RSqAQUyLTQuNbgBA8gBAPgBAcICChAAGEcY1gQYsAPiAwQYACBBiAYBkAYK&sclient=gws-wiz-serp";
+      submitSearch(baseUrl, siteQuery);
+    };
+    const submitZhihuKeyWordsWebsite = () => {
+      const siteQuery =
+        "+inurl%3Azhihu.com&newwindow=1&sca_esv=583929409&biw=1920&bih=931&tbs=qdr%3Am&sxsrf=AM9HkKkO7V2IJ5Ozp4j5EDuyuzBwn-ZMUA%3A1700478902629&ei=tj9bZaiBJrihkPIP9KiF4As&ved=0ahUKEwiotMHHudKCAxW4EEQIHXRUAbwQ4dUDCBA&uact=5&oq=%E9%9D%92%E9%BE%99%E8%84%9A%E6%9C%AC+site%3Agithub.com&gs_lp=Egxnd3Mtd2l6LXNlcnAiHOmdkum-meiEmuacrCBzaXRlOmdpdGh1Yi5jb21IxlxQpxFY6VZwAngBkAEAmAHAAqAB_RSqAQUyLTQuNbgBA8gBAPgBAcICChAAGEcY1gQYsAPiAwQYACBBiAYBkAYK&sclient=gws-wiz-serp";
+      submitSearch(baseUrl, siteQuery);
+    };
+    const submitZhihuSourceWebsite = () => {
+      const siteQuery =
+        "+site%3Azhihu.com&newwindow=1&sca_esv=583929409&biw=1920&bih=931&tbs=qdr%3Am&sxsrf=AM9HkKkO7V2IJ5Ozp4j5EDuyuzBwn-ZMUA%3A1700478902629&ei=tj9bZaiBJrihkPIP9KiF4As&ved=0ahUKEwiotMHHudKCAxW4EEQIHXRUAbwQ4dUDCBA&uact=5&oq=%E9%9D%92%E9%BE%99%E8%84%9A%E6%9C%AC+site%3Agithub.com&gs_lp=Egxnd3Mtd2l6LXNlcnAiHOmdkum-meiEmuacrCBzaXRlOmdpdGh1Yi5jb21IxlxQpxFY6VZwAngBkAEAmAHAAqAB_RSqAQUyLTQuNbgBA8gBAPgBAcICChAAGEcY1gQYsAPiAwQYACBBiAYBkAYK&sclient=gws-wiz-serp";
       submitSearch(baseUrl, siteQuery);
     };
     const searches = ref([
@@ -183,6 +188,20 @@ export default defineComponent({
         placeholder: "你还什么都没输入哦",
         action: submitGitHubSourceWebsite,
       },
+      {
+        key: "Zhihu",
+        title: "限定知乎关键词",
+        model: findKeyWords,
+        placeholder: "你还什么都没输入哦",
+        action: submitZhihuKeyWordsWebsite,
+      },
+      {
+        key: "GitHub",
+        title: "限定知乎来源",
+        model: findKeyWords,
+        placeholder: "你还什么都没输入哦",
+        action: submitZhihuSourceWebsite,
+      },
     ]);
     return {
       findKeyWords,
@@ -201,6 +220,12 @@ export default defineComponent({
         { text: "安卓root", type: "info" },
         { text: "抓包", type: "warning" },
         { text: "代理人抓包", type: "danger" },
+        { text: "微信账号批发", type: "info" },
+        { text: "青龙脚本", type: "warning" },
+        { text: "JS逆向", type: "danger" },
+        { text: "黑产大数据", type: "info" },
+        { text: "黑客新闻", type: "warning" },
+        { text: "加密", type: "danger" },
       ],
       websiteTransformType,
       searches,
@@ -242,5 +267,8 @@ export default defineComponent({
   color: rgb(44, 62, 80);
   font-weight: 600;
   font-size: 21px;
+}
+.tag-common {
+  margin-bottom: 10px;
 }
 </style>
