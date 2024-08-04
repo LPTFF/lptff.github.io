8de534354d1dab4b9006a2531ef87f2892263f63
LPTFF <lptffhh@gmail.com>
2024-08-04 13:48:17 +0800

    删除变量

--- src/crawl/sendNotify.py
+++ src/crawl/sendNotify.py
@@ -1,11 +1,10 @@
 import requests
 import argparse
-QYWX_KEY = "cb675e27-35c5-4b6c-ae64-a7eaac2fdbae"
 
 
 def send_post_request(key):
     # 替换为你的目标URL
-    url = f'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key={QYWX_KEY}'
+    url = f'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key={key}'
     payload = {
         "msgtype": "text",
         "text": {
@@ -15,13 +14,13 @@ def send_post_request(key):
     try:
         # 发送POST请求并接收响应
         response = requests.post(url, json=payload)
+        print('消息推送提醒成功')
     except Exception as e:
-        print('发生异常:', e)
+        print('消息推送提醒发生异常:', e)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--key')
     args = parser.parse_args()
-    print(f'args: {args}')
     send_post_request(args.key)
