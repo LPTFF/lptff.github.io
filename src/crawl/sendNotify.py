5eaad3141921d3e51da46f49e2787453c6a67553
LPTFF <lptffhh@gmail.com>
2024-08-04 14:41:12 +0800

    代码调整

--- src/crawl/sendNotify.py
+++ src/crawl/sendNotify.py
@@ -3,15 +3,10 @@ import argparse
 import os
 
 
-def send_post_request(key):
+def send_post_request(key, msg):
     # 替换为你的目标URL
     url = f'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key={key}'
-    payload = {
-        "msgtype": "text",
-        "text": {
-            "content": "GitHub个人网站脚本刚更新了",
-        }
-    }
+    payload = msg
     try:
         response = requests.post(url, json=payload)
         print('消息推送提醒成功')
@@ -23,11 +18,29 @@ if __name__ == "__main__":
     try:
         ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
         QYWX_KEY = os.getenv('QYWX_KEY')
-        print('ACCESS_TOKEN:', ACCESS_TOKEN)
-        print('QYWX_KEY:', QYWX_KEY)
+        msg = {
+            "msgtype": "template_card",
+            "template_card": {
+                "card_type": "text_notice",
+                "source": {
+                    "icon_url": "https://avatars.githubusercontent.com/u/31006738?v=4",
+                    "desc": "GitHub个人网站",
+                    "desc_color": 0
+                },
+                "jump_list": [
+                    {
+                        "type": 1,
+                        "url": "https://lptff.github.io/",
+                        "title": "随风而逝"
+                    },
+                    {
+                        "type": 1,
+                        "url": "https://github.com/LPTFF",
+                        "title": "GitHub地址"
+                    }
+                ],
+            }
+        }
+        send_post_request(QYWX_KEY, msg)
     except Exception as e:
         print('os.getenv Exception:', e)
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--key')
-    args = parser.parse_args()
-    send_post_request(args.key)
