34aa8f32a00a460b60ddba8db9af79037b81cd0f
LPTFF <lptffhh@gmail.com>
2025-04-20 19:45:05 +0800

    新增消息通知脚本

---
+++ sendNotify.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+# 检查当前操作系统
+echo "OSTYPE  $OSTYPE"
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    # Linux 系统
+    echo "Detected Linux OS"
+    echo "Installing Python..."
+
+    # 在此处添加适用于 Linux 系统的安装命令
+    sudo apt-get update
+    sudo apt-get install python3
+
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    # macOS 系统
+    echo "Detected macOS"
+    echo "Installing Python..."
+
+    # 在此处添加适用于 macOS 系统的安装命令
+    brew update
+    brew install python@3
+elif [[ "$OSTYPE" == "msys"* ]]; then
+    # win 系统
+    echo "Unsupported Windows"
+    # 在此处添加适用于 macOS 系统的安装命令
+    exit 1
+else
+    # 其他操作系统
+    echo "Detected deafault OS"
+    echo "Installing Python..."
+
+    # 在此处添加适用于 Linux 系统的安装命令
+    sudo apt-get update
+    sudo apt-get install python3
+    # exit 1
+
+fi
+
+# 验证安装是否成功
+python_version=$(python --version 2>&1)
+echo "python_version  $python_version"
+if [[ "$python_version" == *"Python"* ]]; then
+    echo "Python installation successful"
+else
+    echo "Python installation failed"
+fi
+# python 环境集成
+pip install requests
+pip install cryptography
+pip install pyOpenSSL
+pip install certifi
+pip install beautifulsoup4
+pip install pytz
+pip install selenium
+pip install argparse
+
+python  ./src/crawl/sendNotify.py 
+
